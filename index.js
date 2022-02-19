248f2e41d089c2a7bf30b5472bb2d092db560d77
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-02-19 15:35:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 const connectToMongo = require("./db");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectToMongo();
 
